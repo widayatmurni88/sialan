8c37b48f42be132a2366ef60b87486c18411ae29
widayatmurni <widayatmurni@gmail.com>
2020-07-23 19:49:54 +0900

    ui dashboard user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,22 +21,51 @@
 
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-7">
-          <!-- Default box -->
-          <div class="card bg-gradient-primary">
-            <div class="card-header border-0">
-              <h3 class="card-title"><b>Kehadiran</b></h3>
-            </div>
-            <div class="card-body">
-              Start creating your amazing application!
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
-        </div>
         <div class="col-md-5">
           <!-- Default box -->
-          <div class="card">
+          <!-- Widget: user widget style 2 -->
+            <div class="card card-widget widget-user-2">
+              <!-- Add the bg color to the header using any of the bg-* classes -->
+              <div class="widget-user-header bg-info">
+                <div class="widget-user-image">
+                  <img class="img-circle elevation-1" src="{{ asset('imgs/profiles/' . session()->get('profil_photo')) }}" alt="User">
+                </div>
+                <!-- /.widget-user-image -->
+                <h3 class="widget-user-username"><b>{{ session()->get('name') }}</b></h3>
+                <h6 class="widget-user-desc">{{ 'Jabatan' }}</h6>
+              </div>
+              <div class="card-body bg-info pt-0">
+                <div class="card mb-0">
+                  <div class="card-body text-dark">
+                    <div class="row border-bottom pb-3">
+                      <div class="col-6"><b>Jadwal Kerja</b></div>
+                      <div class="col-6 text-right">Senin, 20-12-2020</div>
+                    </div>
+                    <div class="group-item row pt-3 pb-3">
+                      <div class="col-12 text-center"><h3 class="mb-0"><b>08:00 - 16:00</b></h3></div>
+                    </div>
+                    <div class="group-item row pt-2 pb-2 border-bottom">
+                      <div class="col-12">
+                        ALamat Kantor
+                      </div>
+                    </div>
+
+                    <div class="group-item row pt-3">
+                      <div class="col-12">
+                        <a href="#" class="btn btn-lg btn-primary btn-block"><i class="fa fa-check-circle-o mr-2"></i> Check In</a>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.widget-user -->
+          <!-- /.card -->
+        </div>
+        <div class="col-md-7">
+          <!-- Default box -->
+          <div class="card card-primary card-outline">
             <div class="card-header">
               <h3 class="card-title">Kegiatan hari ini</h3>
 
