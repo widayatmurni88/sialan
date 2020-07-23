ada8d7b626cf80d0fd2fe9681dc05086c17f0ea2
widayatmurni <widayatmurni@gmail.com>
2020-07-23 11:12:37 +0900

    tambah attribut model Absen

--- app/Models/Absen.php
+++ app/Models/Absen.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Absen extends Model
-{
-    //
+class Absen extends Model{
+    protected $table = 'absens';
+    protected $fillable = ['id', 'nid', 'bio_nid', 'pangkat_id', 'tgl_absen'];
+    public $timestamps = false;
 }
