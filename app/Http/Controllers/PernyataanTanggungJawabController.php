aea55e5dd54d0f34042cb97130596c52d19469be
widayatmurni88 <widayatmurni88991@gmail.com>
2020-08-01 05:23:21 +0900

    view pernyataan tanggung jawab

--- app/Http/Controllers/PernyataanTanggungJawabController.php
+++ app/Http/Controllers/PernyataanTanggungJawabController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PernyataanTanggungJawabController extends Controller
 {
-    //
+    public function index(){
+        $data = [];
+        return view('surattjs')->with($data);
+    }
 }
---
+++ resources/views/surattjs.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.base')
+
+@section('content')
+@include('layout.nav')
+@include('layout.sidebar', ['menu' => 'surattjs'])
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Pernyataan Tanggung Jawab</h1>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title"><i class="fa fa-file mr-3"></i><b>Daftar Pernyataan</b></h3>
+            </div>
+            <div class="card-body">
+              
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+@endsection
